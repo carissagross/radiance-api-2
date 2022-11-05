a2f9ea692e25ee1e4530bd172abd2e314a06041d
Carissa Gross <carissamgross@gmail.com>
2022-11-04 18:43:09 -0600

    update server file

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set('port', port)
 
 //middleware
 app.use(cors())
-app.use(express.json());
+// app.use(express.json());
 
 app.get('/', (request, response) => {
   response.status(200).json({
