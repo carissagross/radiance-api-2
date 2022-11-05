a7fec067d9fd1563bd534ee5965380e15fa075c4
Carissa Gross <carissamgross@gmail.com>
2022-11-04 18:27:19 -0600

    update server fiel

--- server.js
+++ server.js
@@ -23,39 +23,39 @@ app.get('/', (request, response) => {
   })
 })
 
-app.get('/api/v1/affirmations', async(req, res) => {
+app.get('/affirmations', async(req, res) => {
     const affirmationData = await knex.select().from('affirmations')
     res.status(200).json(affirmationData)
 });
 
-app.get('/api/v1/affirmations/:id', (req, res) => {
-  knex('affirmations')
-  .where({ id:  parseInt(req.params.id) })
-  .then((data) => { res.json(data); })
-  .catch(() => { res.json('Something went wrong.') });
-});
+// app.get('/api/v1/affirmations/:id', (req, res) => {
+//   knex('affirmations')
+//   .where({ id:  parseInt(req.params.id) })
+//   .then((data) => { res.json(data); })
+//   .catch(() => { res.json('Something went wrong.') });
+// });
 
-app.post('/api/v1/affirmations', async (request, response) => {
-  const paper = request.body;
+// app.post('/api/v1/affirmations', async (request, response) => {
+//   const paper = request.body;
   
-  for (let requiredParameter of ['description', 'image']) {
-    if (!paper[requiredParameter]) {
-      return response
-      .status(422)
-      .send({ error: `Expected format: { description: <String>, image: <String> }. You're missing a "${requiredParameter}" property.` });
-    }
-  }
+//   for (let requiredParameter of ['description', 'image']) {
+//     if (!paper[requiredParameter]) {
+//       return response
+//       .status(422)
+//       .send({ error: `Expected format: { description: <String>, image: <String> }. You're missing a "${requiredParameter}" property.` });
+//     }
+//   }
   
-  try {
-    const id = await knex.select().from('affirmations').insert(paper, 'id');
-    response.status(201).json({ id })
-  } catch (error) {
-    response.status(500).json({ error });
-  }
-});
+//   try {
+//     const id = await knex.select().from('affirmations').insert(paper, 'id');
+//     response.status(201).json({ id })
+//   } catch (error) {
+//     response.status(500).json({ error });
+//   }
+// });
 
-app.listen(port, () => {
-    console.log(`Listening on ${port}`)
+app.listen(app.get('port'), () => {
+  console.log(`server has started on http://localhost:${app.get('port')}.`)
 })
 
 //    app.post('/affirmations', (req, res) => {
