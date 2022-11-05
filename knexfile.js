7c90a9fcd15a93d9543b10a75e42d9ad7c9c752c
Carissa Gross <carissamgross@gmail.com>
2022-11-04 19:26:48 -0600

    update sever file

--- knexfile.js
+++ knexfile.js
@@ -10,21 +10,21 @@ module.exports = {
     connection: {
       port: 5432,
       database: 'radiance_api',
-      user:     'process.env.DB_USER',
+      user:     process.env.DB_USER,
       password: 'Hike9829'
     },
   },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    },
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
+    // migrations: {
+    //   tableName: 'knex_migrations'
+    // },
     production: {
       client: 'postgresql',
       connection: {
-        connectionString: process.env.HEROKU_POSTGRESQL_PUCE_URL, 
+        connectionString: process.env.DATABASE_URL, 
         ssl: {
         rejectUnauthorized: false,
       },
