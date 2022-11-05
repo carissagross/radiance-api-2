d4f0da8f6088d9ae744898072ad7dcb0a27dd814
Carissa Gross <carissamgross@gmail.com>
2022-11-04 19:13:34 -0600

    update server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set('port', port)
 
 //middleware
 app.use(cors())
-// app.use(express.json());
+app.use(express.json());
 
 app.get('/', (request, response) => {
   response.status(200).json({
@@ -23,10 +23,9 @@ app.get('/', (request, response) => {
   })
 })
 
-app.get('/affirmations', (req, res) => {
-    const affirmationData = knex.select().from('affirmations')
+app.get('/api/v1/affirmations', async(req, res) => {
+    const affirmationData = await knex.select().from('affirmations')
     res.status(200).json(affirmationData)
-    .catch(error => console.log(error))
 });
 
 // app.get('/api/v1/affirmations/:id', (req, res) => {
